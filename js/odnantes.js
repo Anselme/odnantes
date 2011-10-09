9328af8cf43d9698f5ab287c02e971f178cfbafb
Anselme <stephane@letsroot.net>
2011-10-09 15:25:24 +0200

    js pour afficher détails des communes

--- js/odnantes.js
+++ js/odnantes.js
@@ -1,9 +1,14 @@
-$(function(){
-    $('.before_after_container').beforeAfter({
+$('.before_after_container').beforeAfter({
 
-        beforeLinkText: 1960,
-        afterLinkText: 2011
+    beforeLinkText: 1960,
+    afterLinkText: 2011
 
-    });
+});
+
+$('.details').click(function(){
+    src = $(this).attr("href");
+    $('.communes_details').find("> img").attr('src', src);
+    $('.communes_details').show("slow");
+    return false;
 });
 
