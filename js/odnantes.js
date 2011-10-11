6214d399de7f19b586c271c9267f4cb3c04632aa
Anselme <stephane@letsroot.net>
2011-10-11 13:43:57 +0200

    css communes

--- js/odnantes.js
+++ js/odnantes.js
@@ -1,14 +1,16 @@
 $('.before_after_container').beforeAfter({
 
     beforeLinkText: 1960,
-    afterLinkText: 2011
+    afterLinkText: 2008
 
 });
 
+/**
 $('.details').click(function(){
     src = $(this).attr("href");
     $('.communes_details').find("> img").attr('src', src);
     $('.communes_details').show("slow");
     return false;
 });
+*/
 
